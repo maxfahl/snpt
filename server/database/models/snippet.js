3834f2a5444d69ed10ae85b6018edd3f290a6c5b
maxfahl <max@pixilab.se>
2020-09-19 10:07:38 +0200

    Basic models

---
+++ server/database/models/snippet.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+	Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+	class Snippet extends Model {
+		static associate(models) {
+			Snippet.belongsTo(models.User, {
+				foreignKey: 'userId',
+				as: 'user',
+				onDelete: 'CASCADE',
+			})
+		}
+	};
+	Snippet.init({
+		name: DataTypes.STRING,
+		content: DataTypes.STRING,
+		userId: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+		},
+	}, {
+		sequelize,
+		modelName: 'Snippet',
+	});
+	return Snippet;
+};
---
+++ server/database/models/user.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+	Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+	class User extends Model {
+		static associate(models) {
+			User.hasMany(models.Snippet, {
+				foreignKey: 'userId',
+				as: 'snippets',
+				onDelete: 'CASCADE',
+			});
+		}
+	};
+	User.init({
+		name: DataTypes.STRING,
+		email: DataTypes.STRING,
+		password: DataTypes.STRING
+	}, {
+		sequelize,
+		modelName: 'User',
+	});
+	return User;
+};
